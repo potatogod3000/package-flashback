4a98183a0228015af5c5df4c360480bb15a63578
Samuel Ramesh <sampingu02@gmail.com>
2023-08-07 11:16:50 +0530

    commandline argument handling enhancements.

--- Source Files/main.cpp
+++ Source Files/main.cpp
@@ -2,24 +2,25 @@
 #include "os.h"
 
 void printWrongUsage(int argc, char **argv) {
-    std::cout << "WRONG USAGE: \'package-flashback";
+    std::cout << "WRONG USAGE: package-flashback";
     for(int i = 1; i < argc; i++) {
-        std::cout << " " << argv[i];
+        if(i == 1) std::cout << " " << argv[i];
+        else std::cout << " \"" << argv[i] << "\"";
     }
-    std::cout << "\'\n\n";
+    std::cout << "\n\n";
     std::cout << "USAGE: package-flashback <arguments>\n";
-    std::cout << "Arguments:\n-c --> Check the OS.\n-i --> Show installed packages count.\n-p --> Show all the installed packages.\n";
+    std::cout << "Arguments:\n-c  --> Check the OS.\n-i  --> Show installed packages count.\n-p  --> Show all the installed packages.\n";
     std::cout << "-se --> Search the entered string against all the installed packages.\n";
-    std::cout << "-sv --> Save the installed packages to \'packagesInstalled.txt\' file in \"$XDG_DATA_HOME/package-flashback\"." << std::endl;
+    std::cout << "-sv --> Save the installed packages to \"$XDG_DATA_HOME/package-flashback/packagesInstalled.txt\"." << std::endl;
 }
 
 void printHelp() {
     std::cout << "|| Welcome to package-flashback ||\n";
     std::cout << "|| HELP ||\n\n";
     std::cout << "USAGE: package-flashback <arguments>\n";
-    std::cout << "Arguments:\n-c --> Check the OS.\n-i --> Show installed packages count.\n-p --> Print all the installed packages.\n";
+    std::cout << "Arguments:\n-c  --> Check the OS.\n-i  --> Show installed packages count.\n-p  --> Print all the installed packages.\n";
     std::cout << "-se --> Search the entered string against all the installed packages.\n";
-    std::cout << "-sv --> Save the installed packages to \'packagesInstalled.txt\' file in \"$XDG_DATA_HOME/package-flashback\".\n\n";
+    std::cout << "-sv --> Save the installed packages to \"$XDG_DATA_HOME/package-flashback/packagesInstalled.txt\".\n\n";
     std::cout << "Example: package-flashback -se \"kernel\"" << std::endl;
 }
 
@@ -34,16 +35,13 @@ int main(int argc, char **argv) {
     }
     
     else {
-        std::cout << "|| Welcome to package-flashback ||\n\n";
-        
-        std::string name;
-        bool check = operatingSystemCheck();
-        
-        if(check)
-            name = osName();
-        
-        
         if(strcmp(argv[1], "-i") == 0 || strcmp(argv[1], "-p") == 0 || strcmp(argv[1], "-se") == 0 || strcmp(argv[1], "-sv") == 0) {
+            std::cout << "|| Welcome to package-flashback ||\n\n";
+            
+            std::string name;
+            bool check = operatingSystemCheck(); 
+            if(check) name = osName();
+            
             linuxSystem(check, name, argv);
         }
 
